31910c637912f9b89572dfcc129c7183771cb10f
segfaultsourcery <segfaultsourcery@users.noreply.github.com>
2025-11-09 21:44:15 +0100

    Add comments

--- buffin/src/lib.rs
+++ buffin/src/lib.rs
@@ -14,36 +14,44 @@ pub struct Buffin<'a> {
 
 #[allow(clippy::result_unit_err)]
 impl<'a> Buffin<'a> {
+    /// Create a new instance with an empty buffer.
     pub fn new(buffer: &'a mut [u8]) -> Self {
         Self { buffer, pos: 0 }
     }
 
+    /// Create a new instance that considers itself filled by the provided buffer.
     pub fn new_filled(buffer: &'a mut [u8]) -> Self {
         let len = buffer.len();
         Self { buffer, pos: len }
     }
 
+    /// Create a new instance that considers the first `pos` bytes as data.
     pub fn with_pos(buffer: &'a mut [u8], pos: usize) -> Self {
         Self { buffer, pos }
     }
 
+    /// Returns the number of used bytes.
     pub fn len(&self) -> usize {
         self.pos
     }
 
+    /// Returns the used bytes as a slice.
     pub fn bytes(&'a self) -> &'a [u8] {
         &self.buffer[..self.pos]
     }
 
+    /// Empty the buffer.
     pub fn clear(&mut self) {
         self.pos = 0;
     }
 
+    /// Returns true if the buffer is empty.
     #[must_use]
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Adds the given bytes as is.
     pub fn add_bytes(&mut self, bytes: &[u8]) -> Result<()> {
         if self.pos + bytes.len() >= self.buffer.len() {
             bail!("Buffer is too small");
@@ -55,17 +63,19 @@ impl<'a> Buffin<'a> {
         Ok(())
     }
 
+    /// Adds something that implements ToBytes.
     pub fn add<T: ToBytes>(&mut self, b: &T) -> Result<()> {
         self.pos += b.to_bytes(&mut self.buffer[self.pos..])?;
         Ok(())
     }
 
-    /// Remove the n first values.
+    /// Remove the n first bytes.
     pub fn remove_first(&mut self, n: usize) {
         self.buffer.copy_within(n..self.pos, 0);
         self.pos -= n;
     }
 
+    /// Attempts to pop the first item of the given type.
     pub fn pop<T: FromBytes>(&mut self) -> Result<T, PopFailure> {
         match T::from_bytes(self.bytes()) {
             Ok((remainder, result)) => {
